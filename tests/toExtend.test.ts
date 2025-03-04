87b8a3b8810fe5557ebf0e3297fb81aca2c91d8d
Arya Emami <aryaemami59@yahoo.com>
2025-03-04 02:20:22 -0600

    Remove `transformTestUtils.ts`

--- tests/toExtend.test.ts
+++ tests/toExtend.test.ts
@@ -1,27 +1,94 @@
+import { applyTransform } from 'jscodeshift/src/testUtils.js'
 import * as fs from 'node:fs/promises'
 import * as path from 'node:path'
 import { parser, toExtendTransform } from '../src/transforms/toExtend.js'
-import { runTransformTest } from '../transformTestUtils.js'
 import {
   fixtureParentDirectoryPath,
   runCodemodCLI,
   tempParentDirectoryPath,
 } from './test-utils.js'
 
-const testName = path.basename(import.meta.filename, '.test.ts')
+const transformerName = path.basename(import.meta.filename, '.test.ts')
 
-const fixtureDirectoryPath = path.join(fixtureParentDirectoryPath, testName)
+const fixtureDirectoryPath = path.join(
+  fixtureParentDirectoryPath,
+  transformerName,
+)
 
-const tempDirectoryPath = path.join(tempParentDirectoryPath, testName)
+const tempDirectoryPath = path.join(tempParentDirectoryPath, transformerName)
 
 describe(toExtendTransform, () => {
-  runTransformTest(testName, toExtendTransform, parser, fixtureDirectoryPath)
+  describe(transformerName, async () => {
+    const { globby } = await import('globby')
+
+    const entries = await globby('**/*.input.*', {
+      cwd: fixtureDirectoryPath,
+      absolute: true,
+      objectMode: true,
+    })
+
+    await Promise.all(
+      entries.map(async ({ name: fileName, path: testInputPath }) => {
+        const extension = path.extname(fileName)
+
+        const testName = path.basename(fileName, `.input${extension}`)
+
+        const inputFilePath = path.join(
+          fixtureDirectoryPath,
+          `${testName}.input${extension}`,
+        )
+
+        const outputFilePath = path.join(
+          fixtureDirectoryPath,
+          `${testName}.output${extension}`,
+        )
+
+        const inputFileContent = await fs.readFile(inputFilePath, {
+          encoding: 'utf-8',
+        })
+
+        const expectedOutput = await fs.readFile(outputFilePath, {
+          encoding: 'utf-8',
+        })
+
+        describe(`${testName}${extension}`, () => {
+          it('transforms correctly', () => {
+            const actualOutput = applyTransform(
+              { default: toExtendTransform, parser },
+              {},
+              {
+                path: testInputPath,
+                source: inputFileContent,
+              },
+              { parser },
+            )
+
+            expect(actualOutput).toBe(expectedOutput.trim())
+          })
+
+          it('is idempotent', () => {
+            const actualOutput = applyTransform(
+              { default: toExtendTransform, parser },
+              {},
+              {
+                path: testInputPath,
+                source: inputFileContent,
+              },
+              { parser },
+            )
+
+            expect(actualOutput).toBe(expectedOutput.trim())
+          })
+        })
+      }),
+    )
+  })
 })
 
 describe('cli test', () => {
   test('glob patterns work', async () => {
     await expect(
-      runCodemodCLI([testName, './**/*-*.*.{m,c,}ts{x,}'], {
+      runCodemodCLI([transformerName, './**/*-*.*.{m,c,}ts{x,}'], {
         cwd: tempDirectoryPath,
       }),
     ).resolves.not.toThrow()
